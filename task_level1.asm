8d7201df9a4bdb2bce639761dbf631ffa557415f
xrpusher <60086412+xrpusher@users.noreply.github.com>
2024-12-18 01:44:25 +0400

    Update task_level1.asm

--- task_level1.asm
+++ task_level1.asm
@@ -1,121 +1,124 @@
-;------------------------------------------
-; Константы и настройки
-;------------------------------------------
-        .include "m8def.inc"       ; Включаем стандартный header для ATmega8
+;===========================================================
+; AVR Assembler Project для ATmega8
+; Стандартная сложность: Два таймера выводят строки через USART
+;===========================================================
 
-        .equ F_CPU = 16000000
-        .equ BAUD = 9600
-        ; UBRR = (F_CPU/(16*BAUD))-1
-        .equ UBRR_VALUE = ((F_CPU/(16*BAUD))-1)
+.include "m8def.inc"
 
-        .equ TIMER1_INTERVAL = 62500 ; Примерно 1с при prescaler=256 (16МГц)
-        .equ TIMER2_INTERVAL = 250   ; Пример, интервал для Timer2 (CTC)
+;===========================================================
+; Константы
+;===========================================================
+.equ F_CPU = 16000000           ; Тактовая частота 16 МГц (Arduino Uno)
+.equ BAUD = 9600                ; Скорость UART
+.equ UBRR_VALUE = ((F_CPU/(16*BAUD))-1)
 
-        .def temp = r16
-        .def str_ptr_lo = r30
-        .def str_ptr_hi = r31
+; Интервалы для таймеров
+; Пример: Timer1_INTERVAL = 62500 дает примерно 1 секунду при прескалере 256 и 16 МГц
+.equ TIMER1_INTERVAL = 62500    
+; Timer2_INTERVAL = 250 с прескалером 1024 дает около 16 мс
+.equ TIMER2_INTERVAL = 250      
 
-        .cseg
-        .org 0x0000
-        rjmp main
-        .org OC1Aaddr
-        rjmp TIMER1_COMPA_vect
-        .org OC2addr
-        rjmp TIMER2_COMP_vect
-
-;------------------------------------------
-; Секции данных
-;------------------------------------------
-        .dseg
+;===========================================================
+; Секция данных
+;===========================================================
+.dseg
 ping_str: .db "ping\r\n",0
 pong_str: .db "pong\r\n",0
+;===========================================================
+; Векторы прерываний для ATmega8
+;===========================================================
+.cseg
+.org 0x0000
+    rjmp main                    ; Reset вектор
 
-;------------------------------------------
-; Код программы
-;------------------------------------------
-        .cseg
+.org 0x001A                     ; Timer1 Compare Match A
+    rjmp TIMER1_COMPA_vect
+
+.org 0x0020                     ; Timer2 Compare Match
+    rjmp TIMER2_COMP_vect
+
+;===========================================================
+; Главная программа
+;===========================================================
 main:
-        ; Инициализация стека
-        ldi temp, high(RAMEND)
-        out SPH, temp
-        ldi temp, low(RAMEND)
-        out SPL, temp
+    ; Инициализация стека
+    ldi r16, high(RAMEND)
+    out SPH, r16
+    ldi r16, low(RAMEND)
+    out SPL, r16
 
-        ; Настройка USART (8N1, например на 9600 бод)
-        ldi temp, high(UBRR_VALUE)
-        out UBRRH, temp
-        ldi temp, low(UBRR_VALUE)
-        out UBRR, temp
-        ldi temp, (1<<RXEN)|(1<<TXEN) ; Включаем RX и TX
-        out UCSRB, temp
-        ldi temp, (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0) ; 8 бит, 1 стоп-бит
-        out UCSRC, temp
+    ; Инициализация USART
+    ldi r16, high(UBRR_VALUE)
+    out UBRRH, r16
+    ldi r16, low(UBRR_VALUE)
+    out UBRRL, r16
+    ldi r16, (1<<RXEN)|(1<<TXEN)       ; Включаем прием и передачу
+    out UCSRB, r16
+    ldi r16, (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0) ; 8 бит данных, 1 стоп-бит
+    out UCSRC, r16
 
-        ; Настройка Timer1 в режиме CTC
-        ; Prescaler 256, сравнение с OCR1A
-        ldi temp, high(TIMER1_INTERVAL)
-        sts OCR1AH, temp
-        ldi temp, low(TIMER1_INTERVAL)
-        sts OCR1AL, temp
-        ldi temp, (1<<WGM12)|(1<<CS12) ; CTC Mode, prescaler = 256
-        out TCCR1B, temp
+    ; Настройка Timer1 (CTC режим, TOP = OCR1A, предделитель = 256)
+    ldi r16, high(TIMER1_INTERVAL)
+    sts OCR1AH, r16
+    ldi r16, low(TIMER1_INTERVAL)
+    sts OCR1AL, r16
+    ldi r16, (1<<WGM12)|(1<<CS12)
+    out TCCR1B, r16
 
-        ; Включаем прерывание по совпадению Timer1
-        ldi temp, (1<<OCIE1A)
-        out TIMSK, temp
+    ; Настройка Timer2 (CTC режим, TOP = OCR2, предделитель = 1024)
+    ldi r16, TIMER2_INTERVAL
+    out OCR2, r16
+    ldi r16, (1<<WGM21)|(1<<CS22)|(1<<CS20)
+    out TCCR2, r16
 
-        ; Настройка Timer2 в режиме CTC
-        ; Prescaler 1024 для примера
-        ldi temp, TIMER2_INTERVAL
-        out OCR2, temp
-        ldi temp, (1<<WGM21)|(1<<CS22)|(1<<CS20) ; CTC, prescaler 1024
-        out TCCR2, temp
+    ; Включаем прерывания Timer1 Compare A и Timer2 Compare
+    ldi r16, (1<<OCIE1A)|(1<<OCIE2)
+    out TIMSK, r16
 
-        ; Включаем прерывание по совпадению Timer2
-        in temp, TIMSK
-        ori temp, (1<<OCIE2)
-        out TIMSK, temp
-
-        sei
+    ; Включаем глобальные прерывания
+    sei
 
 main_loop:
-        rjmp main_loop
+    rjmp main_loop
 
-;------------------------------------------
-; Процедуры
-;------------------------------------------
+;===========================================================
+; Подпрограмма отправки строки по USART
+;===========================================================
 send_string:
-        ld temp, Z+
-        tst temp
-        breq send_done
+    ld r16, Z+
+    cpi r16, 0
+    breq send_done
 wait_udr_empty:
-        sbis UCSRA, UDRE
-        rjmp wait_udr_empty
-        out UDR, temp
-        rjmp send_string
+    sbis UCSRA, UDRE
+    rjmp wait_udr_empty
+    out UDR, r16
+    rjmp send_string
 
 send_done:
-        ret
+    ret
 
-;------------------------------------------
-; Обработчики прерываний
-;------------------------------------------
+;===========================================================
+; Обработчик прерывания Timer1 Compare Match A
+;===========================================================
 TIMER1_COMPA_vect:
-        push r30
-        push r31
-        ldi r30, low(ping_str)
-        ldi r31, high(ping_str)
-        rcall send_string
-        pop r31
-        pop r30
-        reti
+    push r30
+    push r31
+    ldi ZL, low(ping_str)
+    ldi ZH, high(ping_str)
+    rcall send_string
+    pop r31
+    pop r30
+    reti
 
+;===========================================================
+; Обработчик прерывания Timer2 Compare Match
+;===========================================================
 TIMER2_COMP_vect:
-        push r30
-        push r31
-        ldi r30, low(pong_str)
-        ldi r31, high(pong_str)
-        rcall send_string
-        pop r31
-        pop r30
-        reti
+    push r30
+    push r31
+    ldi ZL, low(pong_str)
+    ldi ZH, high(pong_str)
+    rcall send_string
+    pop r31
+    pop r30
+    reti
